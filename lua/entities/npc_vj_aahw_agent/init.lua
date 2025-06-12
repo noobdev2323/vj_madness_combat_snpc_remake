b7be76a6c2000be3209d5e16c2f58d2842ea397a
noob_dev2323 <147894264+noobdev2323@users.noreply.github.com>
2025-06-11 23:11:36 -0300

    NEW NPC

--- lua/entities/npc_vj_aahw_agent/init.lua
+++ lua/entities/npc_vj_aahw_agent/init.lua
@@ -7,7 +7,7 @@ include('shared.lua')
 -----------------------------------------------*/
 ENT.Model = {"models/noob_dev2323/madness/npc/grunt_npc.mdl"} -- The game will pick a random model from the table when the SNPC is spawned | Add as many as you want
 ENT.StartHealth = 20
-
+ENT.Weapon_Disabled = false   -- Disable the ability for it to use weapons
 ENT.VJ_NPC_Class = {"CLASS_AAHW"} -- NPCs with the same class with be allied to each other
   
 ---------------------------------------------------------------------------------------------------------------------------------------------
@@ -15,33 +15,8 @@ function ENT:CustomOnInitialize()
 	self.totalDamage = {}
 	self.GetDamageType = {} --need to gib work
 	self.gib_type = "ok"
-	self.melee = true 
-	local melee = math.random(1,4)
-	if melee == 1 then
-		bonemerge_prop_on_npc("models/noob_dev2323/madness/weapons/w_bowieKnife.mdl",self)
-		self.MeleeAttackDamage = 35
-		self.MeleeAttackDamageType = DMG_SLASH
-		self.AnimTbl_MeleeAttack = {"vjseq_stab","vjseq_melee_attack_01"}
-		self.SoundTbl_MeleeAttack = {"noob_dev2323/madness/melee/slash_01.wav","noob_dev2323/madness/melee/slash_02.wav","noob_dev2323/madness/melee/slash_03.wav","noob_dev2323/madness/melee/slash_04.wav"}
-	elseif melee == 2 then
-		bonemerge_prop_on_npc("models/noob_dev2323/madness/weapons/w_iron_knife.mdl",self)
-		self.MeleeAttackDamage = 35
-		self.MeleeAttackDamageType = DMG_SLASH
-		self.AnimTbl_MeleeAttack = {"vjseq_stab","vjseq_melee_attack_01"}
-		self.SoundTbl_MeleeAttack = {"noob_dev2323/madness/melee/slash_01.wav","noob_dev2323/madness/melee/slash_02.wav","noob_dev2323/madness/melee/slash_03.wav","noob_dev2323/madness/melee/slash_04.wav"}
-	elseif melee == 3 then
-		bonemerge_prop_on_npc("models/noob_dev2323/madness/weapons/w_bat.mdl",self)
-		self.MeleeAttackDamage = 35
-		self.MeleeAttackDamageType = DMG_CLUB
-		self.AnimTbl_MeleeAttack = {"vjseq_melee_attack_01"}
-		self.SoundTbl_MeleeAttack = {"noob_dev2323/madness/melee/Hit_1.wav","noob_dev2323/madness/melee/Hit_2.wav","noob_dev2323/madness/melee/Hit_3.wav"}
-	elseif melee == 4 then
-		bonemerge_prop_on_npc("models/noob_dev2323/madness/weapons/w_hammer.mdl",self)
-		self.MeleeAttackDamage = 20
-		self.MeleeAttackDamageType = DMG_CLUB
-		self.AnimTbl_MeleeAttack = {"vjseq_melee_attack_01"}
-		self.SoundTbl_MeleeAttack = {"noob_dev2323/madness/melee/Hit_1.wav","noob_dev2323/madness/melee/Hit_2.wav","noob_dev2323/madness/melee/Hit_3.wav"}
-	end
+	self:SetBodygroup(2, 1)
+
 end
 ---------------------------------------------------------------------------------------------------------------------------------------------
 	-- All functions and variables are located inside the base files. It can be found in the GitHub Repository: https://github.com/DrVrej/VJ-Base
